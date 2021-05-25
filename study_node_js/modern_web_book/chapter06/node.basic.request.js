ea3882e3d6cdcb99536d1a01d97edf122ec679a6
J.ian <jay2u8809@gmail.com>
2021-05-25 09:13:50 +0900

    Feat. Add request object

---
+++ study_node_js/modern_web_book/chapter06/node.basic.request.js
@@ -0,0 +1,28 @@
+/**
+ * リクエスト
+ */
+
+// Modules
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+http.createServer((req, res) => {
+
+    const pathname = url.parse(req.url).pathname;
+
+    if (pathname == '/') {
+        fs.readFile('../media/HTMLPage.html', (err, data) => {
+            res.writeHead(200, {    'Content-Type': 'text/html' });
+            res.end(data);
+        });
+    } else if (pathname == '/OtherPage') {
+        fs.readFile('../media/OtherHTMLPage.html', (err, data) => {
+            res.writeHead(200, {    'Content-Type': 'text/html' });
+            res.end(data);
+        });
+    }
+
+}).listen(52273, () => {
+    console.log('Server Running Port Number : 52273');
+});
