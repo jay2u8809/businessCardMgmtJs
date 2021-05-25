1f4d3d115abc9ecaf4364b3f9260db709ae276be
J.ian <jay2u8809@gmail.com>
2021-05-25 09:02:50 +0900

    Fix. move html file

--- study_node_js/modern_web_book/chapter06/node.basic.response.html.js
+++ study_node_js/modern_web_book/chapter06/node.basic.response.html.js
@@ -10,7 +10,7 @@ const http = require('http');
 http.createServer((req, res) => {
 
     // HTML読み込み
-    fs.readFile('./HTMLPage.html', (err, data) => {
+    fs.readFile('../media/HTMLPage.html', (err, data) => {
         res.writeHead(200, {    'Content-Type': 'text/html' });
         res.end(data);
     });
