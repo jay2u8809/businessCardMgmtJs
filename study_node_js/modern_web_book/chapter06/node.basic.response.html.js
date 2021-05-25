00822c2b62206cf11fc8c52ce732eb29bf4292d2
Jung Ian <jay2u8809@gmail.com>
2021-05-25 21:28:43 +0900

    Fix. change file path

--- study_node_js/modern_web_book/chapter06/node.basic.response.html.js
+++ study_node_js/modern_web_book/chapter06/node.basic.response.html.js
@@ -10,7 +10,7 @@ const http = require('http');
 http.createServer((req, res) => {
 
     // HTML読み込み
-    fs.readFile('../media/HTMLPage.html', (err, data) => {
+    fs.readFile('../resources/html/HTMLPage.html', (err, data) => {
         res.writeHead(200, {    'Content-Type': 'text/html' });
         res.end(data);
     });
